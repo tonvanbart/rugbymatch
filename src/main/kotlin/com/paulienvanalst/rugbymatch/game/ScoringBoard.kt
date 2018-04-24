f0cd8aba45a7319221024c66040f18a18e15a979
Ton van Bart <ton.vanbart@kpn.com>
2018-04-24 21:09:42 +0200

    exercise 3 solutions Ton

--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
@@ -11,13 +11,30 @@ open class ScoringBoard {
 
     private var scoringHistory : List<Score> = emptyList()
 
+    private lateinit var hostingTeam : TeamName
+
+    private lateinit var visitingTeam : TeamName
+
     fun currentScore(): GameScore {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val hostScore = scoringHistory.filter { it.forTeam == hostingTeam }.map { it.type.points }.sum()
+        val visitScore = scoringHistory.filter { it.forTeam == visitingTeam }.map { it.type.points }.sum()
+        return GameScore(Pair(hostingTeam, hostScore), Pair(visitingTeam, visitScore))
     }
 
     fun clear () {
         scoringHistory = emptyList()
+    }
 
+    @EventListener
+    fun handleStartGame(startGame: StartGame) {
+        this.hostingTeam = startGame.hostingTeam
+        this.visitingTeam = startGame.visitingTeam
+    }
+
+    @EventListener
+    fun handleScore(scoringEvent: ScoringEvent) {
+        val score = Score(scoringEvent.team, scoringEvent.type)
+        scoringHistory += score
     }
 }
 
